ebf1106d49f9db16ecfb99d9e77ac13956928f56
Laimonas Grinius <la.grinius@gmail.com>
2024-06-25 20:29:57 +0300

    RepeatOrNot method added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
 
-        public enum numberFormatEnum
+        public enum NumberFormatEnum
         {
             Binary = 2,
             Octal = 8,
@@ -19,10 +20,10 @@ namespace ConsoleApp1
             {
                 switch (number)
                 {
-                    case (int)numberFormatEnum.Binary:
-                    case (int)numberFormatEnum.Octal:
-                    case (int)numberFormatEnum.Decimal:
-                    case (int)numberFormatEnum.Hexadecimal:
+                    case (int)NumberFormatEnum.Binary:
+                    case (int)NumberFormatEnum.Octal:
+                    case (int)NumberFormatEnum.Decimal:
+                    case (int)NumberFormatEnum.Hexadecimal:
                         return "goodFormatFromBinToHex";
                     default:
                         if (Convert.ToInt32(formatString) >= 17 && Convert.ToInt32(formatString) <= 99) return "goodFormatFrom17To99";
@@ -43,13 +44,17 @@ namespace ConsoleApp1
             }
             Console.Write("Please enter your number conversion base: \nAllowed types: \nBinary - 2 \nOctal - 8 \nDecimal - 10 \nHexadecimal - 16 \nOther type - 17-99\n");
         }
-        public static string RepeatOrNot(char yesOrNo)
+        public static string RepeatOrNot()
         {
-            if (yesOrNo == 'y') return "yes";
-            if (yesOrNo == 'n') return "no";
+            string yesOrNo;
+            Console.WriteLine("Do you want to enter new number to convert? (y/n): ");
+            yesOrNo = Console.ReadLine();
+            yesOrNo.ToLower();
+            if (yesOrNo == "y") return "yes";
+            if (yesOrNo == "n") return "no";
             else return "wrongEntry";
         }
-        public static List <string> ConvertNumber(Dictionary<int,char> allSymbols, string formatString, string enteredNum, string formatString1) // Converts entered number to decimal and then to estimated base
+        public static List<string> ConvertNumber(Dictionary<int, char> allSymbols, string formatString, string enteredNum, string formatString1) // Converts entered number to decimal and then to estimated base
         {
             List<char> tempNumbers = new List<char>();
             List<string> formatedNum = new List<string>();
@@ -57,37 +62,37 @@ namespace ConsoleApp1
             int tempNum = 0;
             int index = 1;
             int quotNumber;
-            for (int i=0; i<enteredNum.Length;i++)
+            for (int i = 0; i < enteredNum.Length; i++)
             {
                 tempNumbers.Add(enteredNum.ElementAt(i));
             }
-            for(int i = 0; i < tempNumbers.Count; i++)
+            for (int i = 0; i < tempNumbers.Count; i++)
             {
-                foreach(KeyValuePair<int,char> value in allSymbols)
+                foreach (KeyValuePair<int, char> value in allSymbols)
                 {
-                    if (tempNumbers.ElementAt(i)== value.Value)
+                    if (tempNumbers.ElementAt(i) == value.Value)
                     {
                         formatedNum.Add(value.Key.ToString());
                     }
                 }
             }
-            foreach(var value in formatedNum) // Cycle converts entered number to Decimal
+            foreach (var value in formatedNum) // Cycle converts entered number to Decimal
             {
                 tempNum += Convert.ToInt32(value) * (int)Math.Pow(Convert.ToInt32(formatString), formatedNum.Count - index);
                 index++;
-            }  
-            quotNumber=tempNum;
+            }
+            quotNumber = tempNum;
             while (quotNumber != 0) // Cycle converts number to entered format. Example: Decimal to Hexadecimal
             {
                 finishNumbers.Add((quotNumber % Convert.ToInt32(formatString1)).ToString());
                 quotNumber = quotNumber / Convert.ToInt32(formatString1);
             }
             finishNumbers.Reverse();
-            for(int i = 0; i < finishNumbers.Count; i++) // Cycle goes through dictionary 'allSymbols' and when number is equal to 'Key' it gives list 'finishNumbers' value of dictionary value
+            for (int i = 0; i < finishNumbers.Count; i++) // Cycle goes through dictionary 'allSymbols' and when number is equal to 'Key' it gives list 'finishNumbers' value of dictionary value
             {
-                foreach(var value in allSymbols)
+                foreach (var value in allSymbols)
                 {
-                    if (finishNumbers.ElementAt(i)==value.Key.ToString())
+                    if (finishNumbers.ElementAt(i) == value.Key.ToString())
                     {
                         finishNumbers[i] = value.Value.ToString();
                         break;
@@ -96,12 +101,31 @@ namespace ConsoleApp1
             }
             return finishNumbers;
         }
+        /*  public static Dictionary<int,char> AllSymbols()
+          {
+              Dictionary<int, char> allSymbols = new Dictionary<int, char>();
+              int j = 128;
+
+              for(int i=48;i<j;i++)
+              {
+                  if(i==58)
+                  {
+                      i += 7;
+                  }
+                  allSymbols.Add(i, (char)i);
+
+              }
+              return allSymbols;
+          }*/
+
+
         static void Main(string[] args)
         {
             string? errorOrSuccess = "";
             string? inputFormat = "";
-            string? inputFormat1="";
-            string? tempNum;      
+            string? inputFormat1 = "";
+            string? tempNum;
+
 
             while (errorOrSuccess != "goodFormatFromBinToHex" && errorOrSuccess != "goodFormatFrom17To99")
             {
@@ -125,22 +149,36 @@ namespace ConsoleApp1
             {
                 Console.Write(value);
             }
-            Console.ReadKey();
+           
+           /* while (RepeatOrNot() != "wrongEntry")
+            {
+                RepeatOrNot();
+                if(RepeatOrNot()=="n")
+                {
+                    Console.WriteLine("Program ended...");
+                }
+                if(RepeatOrNot()=="y")
+                {
+                    
+                }
 
+            }*/
+            Console.ReadKey();
         }
+      
+
         public static Dictionary<int, char> allSymbols = new Dictionary<int, char>()
     {
-        { 0, '0' },{ 1, '1' },{ 2, '2' },{ 3, '3' },{ 4, '4' },{ 5, '5' },{ 6, '6' },
+       { 0, '0' },{ 1, '1' },{ 2, '2' },{ 3, '3' },{ 4, '4' },{ 5, '5' },{ 6, '6' },
         { 7, '7' },{ 8, '8' },{ 9, '9' },{ 10, 'A' },{ 11, 'B' },{ 12, 'C' },{ 13, 'D' },
         { 14, 'E' },{ 15, 'F' },{ 16, 'G' },{ 17, 'H' },{ 18, 'I' },{ 19, 'J' },{ 20, 'K' },
         { 21, 'L' },{ 22, 'M' },{ 23, 'N' },{ 24, 'O' },{ 25, 'P' },{ 26, 'Q' },{ 27, 'R' },
         { 28, 'S' },{ 29, 'T' },{ 30, 'U' },{ 31, 'V' },{ 32, 'W' },{ 33, 'X' },{ 34, 'Y' },
-        { 35, 'Z' }
-    };
-    
+        { 35, 'Z' }, { 36, '['}, { 37, '\\'}
 
+    };
     }
-}
+} 
     
 
 
